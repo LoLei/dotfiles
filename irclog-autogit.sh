4412dad84c87fdbf5519641da5537d714a37960f
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2016-04-14 16:28:12 +0200

    Update autopusher

--- irclog-autogit.sh
+++ irclog-autogit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ~/irclogs
-inotifywait -q -m -e CLOSE_WRITE --format="./git-commit-and-push.sh" \#bs.log | sh
+inotifywait -q -m -e CLOSE_WRITE --format="./git-commit-and-push.sh" shells/\#bs.log | sh
