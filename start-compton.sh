95677db953ea3879cb9d02536139659fd83be72c
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2016-05-25 21:42:32 +0200

    Re-enable screen tear fix in compton

--- start-compton.sh
+++ start-compton.sh
@@ -1,2 +1,2 @@
-# compton -c -r 20 -o 1 -l -30 -t -30 -z -C -f -D 3 --backend glx --vsync opengl-swc
-compton -c -r 20 -o 1 -l -30 -t -30 -z -C -f -D 3
+compton -c -r 20 -o 1 -l -30 -t -30 -z -C -f -D 3 --backend glx --vsync opengl-swc
+# compton -c -r 20 -o 1 -l -30 -t -30 -z -C -f -D 3
