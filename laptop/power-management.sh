af066ffbd68153df1d9942937ad9635d4b544b76
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2016-12-02 15:29:57 +0100

    Add echos

--- laptop/power-management.sh
+++ laptop/power-management.sh
@@ -6,17 +6,22 @@ cd /sys/class/power_supply/BAT0
 CHARGING="$(cat uevent | grep STATUS | grep -q Charging)"
 echo $CHARGING
 if cat uevent | grep STATUS | grep -q Charging; then
-  echo "AC"
+  echo "Computer is on AC"
+  echo "Setting backlight to 100%..."
   xbacklight -set 100
+  echo "Increasing CPU usage..."
   ~/git/dotfiles/laptop/fast-cpu
 else
-  echo "Draining"
+  echo "Computer battery is draining"
   # Check battery level
   PERCENTAGE="$(cat uevent | grep "CAPACITY=" | grep -Eo '[0-9]{1,4}')"
-  echo $PERCENTAGE
+  echo "Battery percentage:" $PERCENTAGE"%"
+  echo "Setting backlight to 50%..."
   xbacklight -set 50
+  echo "Decreasing CPU usage..."
   ~/git/dotfiles/laptop/slow-cpu
   if [ $PERCENTAGE -lt 25 ]; then
+  echo "Setting backlight to 25%..."
     xbacklight -set 25
   fi
 fi
