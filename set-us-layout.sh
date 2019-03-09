aa3fc7bfb9f4d87ded90a4e214ff66e1b3b63fd1
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-03-09 09:22:25 +0100

    Only set US layout if US peripherals are found

--- set-us-layout.sh
+++ set-us-layout.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-setxkbmap -layout us
-setxkbmap -option compose:rctrl
+
+if lsusb | grep -q Razer; then
+    echo Found Razer peripherals, setting keyboard layout to US
+    setxkbmap -layout us
+    setxkbmap -option compose:ralt
+else
+    echo Did not find Razer peripherals
+fi
