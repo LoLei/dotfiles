57fe8ed8aaa35c7e58446f1cffcc4b7be913776e
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-03-11 18:02:56 +0100

    First draft of keyboard color script

---
+++ set-keyboard-colors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get current primary color used by pywal
+RGB="$(xrdb -query | grep "*color1:" | awk -F '#' '{print $2}')"
+echo $RGB
+
+# Split by RGB
+R=${RGB:0:2}
+echo $R
+G=${RGB:2:2}
+echo $G
+B=${RGB:4:2}
+echo $B
+
+# Go to device drive directory 
+# https://github.com/openrazer/openrazer/wiki/Using-the-keyboard-driver
+cd /sys/bus/hid/drivers/razerkbd/0003:1532:021A.0008
+
+# Write color values into driver file
+echo -n -e "\xFF\xFF\xFF" > matrix_effect_static
