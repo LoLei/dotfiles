fe232be46357547c17ad28a61d0850f971d8310e
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-03-12 16:46:11 +0100

    Add alternative keyboard color script in python

---
+++ set-keyboard-color.py
@@ -0,0 +1,20 @@
+from openrazer.client import DeviceManager
+from openrazer.client import constants as razer_constants
+
+# Create a DeviceManager. This is used to get specific devices
+device_manager = DeviceManager()
+
+print("Found {} Razer devices".format(len(device_manager.devices)))
+print()
+
+# Disable daemon effect syncing.
+# Without this, the daemon will try to set the lighting effect to every device.
+device_manager.sync_effects = False
+
+# Iterate over each device and set the wave effect
+for device in device_manager.devices:
+    print("Setting {} to static".format(device.name))
+
+    # Set the effect to wave.
+    # wave requires a direction, but different effect have different arguments.
+    device.fx.static(255,0,0)
