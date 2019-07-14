1341d7fc030faac47df8c752802af558daf16d37
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-07-14 09:52:02 +0200

    Add script for changing GTK theme

---
+++ change-gtk-theme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Exec pywal first
+
+# Generate GTK3 theme
+oomox-cli /opt/oomox/scripted_colors/xresources/xresources2
+
+# Generate icon theme
+/opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh /opt/oomox/scripted_colors/xresources/xresources2
