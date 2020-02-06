d9772cfc05109fc20e6a73e2075c9c0a1178cfc7
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-02-06 17:59:52 +0100

    Add plex script to fix permissions

---
+++ fix-plexconfig.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Change user and group to own user
+FILE=/usr/lib/systemd/system/plexmediaserver.service
+sudo sed -e 's/User=plex/User=$USER/' -e 's/Group=plex/Group=$USER/' -i $FILE
+sudo chown -R $USER:$USER /var/lib/plex
