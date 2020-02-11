ed4475b2b59d2672ab6a4b182bef45cab329df66
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-02-11 16:12:46 +0100

    Fix use of environment variable in sed replace

--- fix-plexconfig.sh
+++ fix-plexconfig.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Change user and group to own user
 FILE=/usr/lib/systemd/system/plexmediaserver.service
-sudo sed -e 's/User=plex/User=$USER/' -e 's/Group=plex/Group=$USER/' -i $FILE
+sudo sed -e 's/User=plex/User='"$USER"'/' -e 's/Group=plex/Group='"$USER"'/' -i $FILE
 sudo chown -R $USER:$USER /var/lib/plex
+sudo systemctl daemon-reload
+sudo systemctl start plexmediaserver.service
