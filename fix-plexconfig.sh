7062f59a86cfea55cfca7e749690fafa06527eaa
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-02-22 17:56:31 +0100

    Use restart instead of start for plex service

--- fix-plexconfig.sh
+++ fix-plexconfig.sh
@@ -4,4 +4,4 @@ FILE=/usr/lib/systemd/system/plexmediaserver.service
 sudo sed -e 's/User=plex/User='"$USER"'/' -e 's/Group=plex/Group='"$USER"'/' -i $FILE
 sudo chown -R $USER:$USER /var/lib/plex
 sudo systemctl daemon-reload
-sudo systemctl start plexmediaserver.service
+sudo systemctl restart plexmediaserver.service
