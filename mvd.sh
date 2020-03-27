2a63b7f186a2f5d6be39bc91464e88fe4029c839
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-03-27 18:39:09 +0100

    Update mvd

--- mvd.sh
+++ mvd.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+if [ -z "$1" ]
+  then
+    mvdir=$(pwd)
+  else
+    mvdir=$1
+fi
 dldir=$HOME/Downloads
-mv $dldir/$(ls -t1 $dldir |  head -n 1) $(pwd)
+mv $dldir/$(ls -t1 $dldir |  head -n 1) $mvdir
