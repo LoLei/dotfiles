1b8c95afe5a39ddb5a3b33b8d7b3db7d978433b6
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-09 13:09:14 +0200

    Improve mpv script

--- scripts/mpvh.sh
+++ scripts/mpvh.sh
@@ -4,31 +4,30 @@ mpv "$1" --input-ipc-server=/tmp/mpvsocket &
 # Save PID for later
 pid=$(echo $!)
 
-# Symlink opened file to mru
+# Symlink first opened file (via new mpv process) to mru
 readlink -f "$1" >> ~/.logs/mpv-history.log
 ln -s "$(readlink -f "$1")" $HOME/Videos/mru/
 
-# Symlink if next file has been loaded via nextfile.lua
+# Handle if next file has been loaded via nextfile.lua
 # (This doesn't restart the mpv process, but simply replaces the previous file
 # in the existing mpv process)
+last_video="placeholer"
 while :
 do
   # See mpvinfo.sh
   current_video=$(mpvinfo)
-
-  # Kill poll check is not accurate, so mpvinfo will run after exit and empty
-  # variable saved - CBA'd to check for that
-  # So save it somwhere else (Append)
-  echo "$current_video" >> /tmp/mpv_cur_vid
+  if [[ $current_video != "" ]]; then
+    last_video=$current_video
+  fi
 
   # Check if process has exited
   if ! kill -s 0 $pid; then
       # Symlink last played file to mru
       break
   fi
-  sleep 5
+  sleep 1
 done
 
-last_video=$(awk '/./{line=$0} END{print line}' /tmp/mpv_cur_vid)
+# Symlink last played video
 readlink -f "$last_video" >> ~/.logs/mpv-history.log
 ln -s "$(readlink -f "$last_video")" $HOME/Videos/mru/
