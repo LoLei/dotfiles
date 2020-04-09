b64ef72217a893c598f5cbe410b5945cad825ee5
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-09 13:15:32 +0200

    Functions

--- scripts/mpvh.sh
+++ scripts/mpvh.sh
@@ -1,33 +1,47 @@
 #!/bin/sh
-mpv "$1" --input-ipc-server=/tmp/mpvsocket &
 
-# Save PID for later
-pid=$(echo $!)
+get_current_mpv_file_name() {
+  file_name=$(mpvinfo)
+  return $file_name
+}
 
-# Symlink first opened file (via new mpv process) to mru
-readlink -f "$1" >> ~/.logs/mpv-history.log
-ln -s "$(readlink -f "$1")" $HOME/Videos/mru/
+symlink_mru_and_save_history() {
+  readlink -f "$1" >> ~/.logs/mpv-history.log
+  ln -s "$(readlink -f "$1")" $HOME/Videos/mru/
+}
 
-# Handle if next file has been loaded via nextfile.lua
-# (This doesn't restart the mpv process, but simply replaces the previous file
-# in the existing mpv process)
-last_video="placeholer"
-while :
-do
-  # See mpvinfo.sh
-  current_video=$(mpvinfo)
-  if [[ $current_video != "" ]]; then
-    last_video=$current_video
-  fi
+main() {
+  mpv "$1" --input-ipc-server=/tmp/mpvsocket &
 
-  # Check if process has exited
-  if ! kill -s 0 $pid; then
-      # Symlink last played file to mru
-      break
-  fi
-  sleep 1
-done
+  # Save PID for later
+  pid=$(echo $!)
 
-# Symlink last played video
-readlink -f "$last_video" >> ~/.logs/mpv-history.log
-ln -s "$(readlink -f "$last_video")" $HOME/Videos/mru/
+  # Symlink first opened file (via new mpv process) to mru
+  symlink_mru_and_save_history $1
+
+  # Handle if next file has been loaded via nextfile.lua
+  # (This doesn't restart the mpv process, but simply replaces the previous file
+  # in the existing mpv process)
+  last_video="placeholer"
+  while :
+  do
+    # See mpvinfo.sh
+    current_video=$(mpvinfo)
+    if [[ $current_video != "" ]]; then
+      last_video=$current_video
+    fi
+
+    # Check if process has exited
+    if ! kill -s 0 $pid; then
+        # Symlink last played file to mru
+        break
+    fi
+
+    sleep 1
+  done
+
+  # Symlink last played video
+  symlink_mru_and_save_history $last_video
+}
+
+main $1
