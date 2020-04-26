89b4b41ea365142cd20fc7502f4985034cfa7290
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-26 09:45:18 +0200

    Update wal script to accept theme or file/dir

--- exec-wal.sh
+++ exec-wal.sh
@@ -1,8 +1,14 @@
 #!/bin/zsh
 
-DIR=$1
-# Generate colors and set terminal theme
-wal -i $DIR -o ~/git/dotfiles/.config/wal/done.sh &&
+if [[ -f "$1" ]] || [[ -d "$1" ]]
+then
+  # Directory or image supplied
+  # Generate colors and set terminal theme
+  wal -i $1 -o ~/git/dotfiles/.config/wal/done.sh &&
+else
+  # Theme name supplied
+  wal --theme $1 -o ~/git/dotfiles/.config/wal/done.sh &&
+fi
 
 # Set keyboard colors
 razer-cli -a
