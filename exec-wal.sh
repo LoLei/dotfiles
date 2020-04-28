829b5bb4e85da3f513807951ae37a080cf8f541e
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-28 09:04:50 +0200

    Third optional parameter for wal script: -l
    For light theme generation

--- exec-wal.sh
+++ exec-wal.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
+## colors-source [hardcoded-colors] [-l]
+
 if [[ -f "$1" ]] || [[ -d "$1" ]]
 then
   # Directory or image supplied
   # Generate colors and set terminal theme
-  wal -i $1 -o ~/git/dotfiles/.config/wal/done.sh &&
+  wal $3 -i $1 -o ~/git/dotfiles/.config/wal/done.sh &&
 else
   # Theme name supplied
-  wal --theme $1 -o ~/git/dotfiles/.config/wal/done.sh &&
+  wal $3 --theme $1 -o ~/git/dotfiles/.config/wal/done.sh &&
 fi
 
 # Set keyboard colors
