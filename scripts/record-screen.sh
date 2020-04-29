1897cb5d201732fdd567b3ee2cbe579cdb10ee78
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-29 15:42:25 +0200

    Optional audio-input

--- scripts/record-screen.sh
+++ scripts/record-screen.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
-# https://www.reddit.com/r/linux/comments/8n3ylp/screen_recording_in_x11_with_ffmpeg/
-# TODO: Dynamic position and size, mic opt-in
-resolution="1280x1080"
+# Call example: ./record-screen.sh 800x600 1
+resolution=$1
+enable_mic=0
 
-# With mic/audio
-# ffmpeg -y \
-# -video_size $resolution \
-# -framerate 30 -f x11grab -i :0.0 \
-# -f pulse -ac 2 -i default \
-# /tmp/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4 \
-# &> /tmp/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.log
+if [ -n "$2" ]
+then
+  enable_mic=$2
+  echo $enable_mic
+fi
 
-# Without sound
+mic_str=""
+if [ "$enable_mic" -eq 1 ]
+then
+  mic_str="-f pulse -ac 2 -i default"
+  echo $mic_str
+fi
+
 ffmpeg -y \
 -video_size $resolution \
 -framerate 30 -f x11grab -i :0.0 \
+$mic_str \
 /tmp/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4 \
 &> /tmp/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.log
