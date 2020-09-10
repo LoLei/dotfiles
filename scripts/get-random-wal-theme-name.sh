0ceba58a4cfdd007c646f8755e5e5f7857da5f90
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-09-10 09:22:29 +0200

    Use input argument in random theme script

--- scripts/get-random-wal-theme-name.sh
+++ scripts/get-random-wal-theme-name.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 cd "$HOME/git/dotfiles/themes"
-shuf -n 1 themes-list.txt | awk '{print $1;}'
+
+# If there's an argument, output only the theme name,
+# else output everything (including optional color)
+
+if [ -z "$1" ]
+then
+  shuf -n 1 themes-list.txt
+  echo "No argument supplied"
+else
+  shuf -n 1 themes-list.txt | awk '{print $1;}'
+fi
