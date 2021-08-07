92c6df4b8dbb9f343922bded47a61e0b3a9c4196
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-08-07 11:51:24 +0200

    Change resolution in random wallpaper script

--- scripts/rdwp.sh
+++ scripts/rdwp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 filename=~/Pictures/unsplash/$(date -I)-$RANDOM.jpg
-wget https://source.unsplash.com/random/2560x1080 -O $filename
+wget https://source.unsplash.com/random/3840x2160 -O $filename
 feh --bg-fill $filename
